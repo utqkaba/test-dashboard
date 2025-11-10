b64e871b46861f010bf7da967b2af76483ebee6a
utqkaba <utqkaba@gmail.com>
2025-11-10 15:21:33 +0300

    sidebar unit test added

---
+++ src/tests/components/Sidebar.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen, within } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Sidebar from "../../components/Sidebar";
+import { describe, it, expect } from "vitest";
+
+describe("Sidebar Component", () => {
+  it("renders sidebar links correctly", () => {
+    render(
+      <MemoryRouter>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    const nav = screen.getByRole("navigation");
+    const navScope = within(nav);
+
+    expect(navScope.getByText(/dashboard/i)).toBeInTheDocument();
+    expect(navScope.getByText(/users/i)).toBeInTheDocument();
+    expect(navScope.getByText(/roles/i)).toBeInTheDocument();
+    expect(navScope.getByText(/my profile/i)).toBeInTheDocument();
+    expect(navScope.getByText(/log out/i)).toBeInTheDocument();
+  });
+
+  it("highlights the active route", () => {
+    // Test için '/users' routeunu aktif yapıyoruz
+    render(
+      <MemoryRouter initialEntries={["/users"]}>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    const usersLink = screen.getByText(/users/i).closest("a");
+
+    // usersLink'in aktif olduğunu belirten sınıf içerip içermediğini kontrol ediyoruz
+    expect(usersLink).toHaveClass(
+      "bg-linear-to-r",
+      "from-stone-100",
+      "to-stone-300"
+    );
+  });
+});
