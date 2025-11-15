c61cb39eea030c30d288ce21414dbbfa45be1cf2
utqkaba <utqkaba@gmail.com>
2025-11-15 23:31:09 +0300

    deleted unnecessary comment

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,13 @@
 describe("Login Page - Successful Login", () => {
   beforeEach(() => {
-    // Uygulamayı aç
     cy.visit("/");
   });
 
   it("should login successfully with correct credentials", () => {
-    // Email inputuna değer gir
     cy.get('input[type="email"]').type("admin@admin");
-
-    // Password inputuna değer gir
     cy.get('input[type="password"]').type("admin.123.");
-
-    // Login butonuna tıkla
     cy.get('button[type="submit"]').click();
-
-    // Başarılı giriş sonrası /welcome sayfasına yönlendirme kontrolü
     cy.url().should("include", "/welcome");
-
-    // /welcome sayfasında kullanıcı email’inin görünmesini kontrol et
     cy.contains("admin@admin").should("be.visible");
   });
 });
